ff249a143d3d56768ec3f727296599321db6aa86
jofigure <jia.lao@stonybrook.edu>
2020-11-01 23:39:49 -0500

    cmitting

--- finder.js
+++ finder.js
@@ -1,10 +1,11 @@
 const MongoClient = require('mongodb').MongoClient;
 const uri = "mongodb+srv://joanna:qwerty1@cluster0.w03kq.mongodb.net/ClassesDB?retryWrites=true&w=majority";
 const client = new MongoClient(uri, { useNewUrlParser: true });
+
 MongoClient.connect(uri, function (err, db) {
   if (err) throw err;
   var dbo = db.db("ClassesDB");
-  var query = { Sctn: "Machine Learning" };
+//var query = { Sctn: "Machine Learning" };
   // dbo.collection("class").find(query).toArray(function (err, result) {
   //   if (err) throw err;
   //   console.log(result);
@@ -24,18 +25,23 @@ app.use(bodyParser());
 app.get('/', (req, res) => {
   res.sendFile('ClassFindHW4.html',{root: __dirname});
   //console.log(req.body.searchText);
-});
+}); //cd C:\Users\joann\112219617_CSE316_HW4
 app.post('/', (req,res) => {
  var name = req.body.searchText;
- if(name == null){
+ if(name == null || name == ""){
+
+  MongoClient.connect(uri, function (err, db) {
+    if (err) throw err;
+    var dbo = db.db("ClassesDB");
    dbo.collection("class").find().toArray(function (err, result) {
     if (err) throw err;
     console.log(result);
     db.close();
-  });;
- }
- console.log('what' + name);
+  });
 });
+  console.log("name is suppose to b empty");}
+});
+
 
 app.listen(PORT, function(err){  if (err) console.log(err); 
 console.log("Server listening on PORT", PORT);
