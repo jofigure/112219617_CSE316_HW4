f87bbc96b0dde9e2b3d5574507604ce4ec2d73e7
jofigure <jia.lao@stonybrook.edu>
2020-11-03 23:21:09 -0500

    working on tab

--- finder.js
+++ finder.js
@@ -23,7 +23,7 @@ const PORT = 3000;
 app.use(express.static(path.join(__dirname, "ClassFindHW4")));
 app.use(bodyParser()); 
 
-var courseScript = new 
+//var courseScript = new 
 
 
 
@@ -31,14 +31,18 @@ app.get('/', (req, res) => {
   res.sendFile('ClassFindHW4.html',{root: __dirname});
   //console.log(req.body.searchText);
 }); //cd C:\Users\joann\112219617_CSE316_HW4
+
+
 app.post('/', (req,res) => {
  var name = req.body.searchText;
- if(name == null || name == ""){
+ if(name == null || name == " " || name == ""){
 
   MongoClient.connect(uri, function (err, db) {
     if (err) throw err;
     var dbo = db.db("ClassesDB");
-   dbo.collection("class").find().toArray(function (err, result) {
+   
+   dbo.collection("class").find({}).toArray(function (err, result) {
+    
     if (err) throw err;
     console.log(result);
     db.close();
@@ -48,6 +52,6 @@ app.post('/', (req,res) => {
 });
 
 
-app.listen(PORT, function(err){  if (err) console.log(err); 
+app.listen(PORT, function(err){  if (err) throw err; 
 console.log("Server listening on PORT", PORT);
 });
